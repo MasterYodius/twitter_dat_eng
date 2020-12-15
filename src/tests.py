25eccdc3b86790b0cafab03e1567f2100e95fd86
Coronavitriol <63202560+coronavitriol@users.noreply.github.com>
2020-12-15 19:47:11 +0100

    Update tests.py

--- src/tests.py
+++ src/tests.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov  6 16:47:30 2020
-
 @author: minimilien
 """
 
@@ -11,21 +10,21 @@ import time as tempo
 
 def test1():
     dictToSend = {'question':'what is the answer?'}
-    res = requests.post('http://localhost:5000/RAM', json=dictToSend)
+    res = requests.post('http://127.0.0.1:5000/RAM', json=dictToSend)
     print('response from server:',res.text)
     print(res)
 
 
 def test2():
     dictToSend = {'question':'what is the answer?'}
-    res = requests.post('http://localhost:5000/horloge', json=dictToSend)
+    res = requests.post('http://127.0.0.1:5000/horloge', json=dictToSend)
     print('response from server:',res.text)
     print(res)
     
     
 def test3():
     dictToSend = {'question':'what is the answer?'}
-    res = requests.post('http://localhost:5000/sfggd', json=dictToSend)
+    res = requests.post('http://127.0.0.1:5000/sfggd', json=dictToSend)
     print ('response from server:',res.text)
     print(res)
 
@@ -38,7 +37,13 @@ def test4():
 
 def test5():
     dictToSend = {'phrase':'wall'}
-    res = requests.post('http://localhost:5000/sentimements', json=dictToSend)
+    res = requests.post('http://127.0.0.1:5000/sentimements', json=dictToSend)
+    print('response from server:',res.text)
+    print(res)
+    
+def test6():
+    dictToSend = {'phrase':'wall'}
+    res = requests.post('http://127.0.0.1:5000/SIM', json=dictToSend)
     print('response from server:',res.text)
     print(res)
 
@@ -77,5 +82,6 @@ print(tempo.time()-t0)
 t0=tempo.time()
 list(map(lambda x : test1(),list(range(1000))))
 print(tempo.time()-t0)
-
-#deploiement(test1,test2,test3,test4,test5)
+t0=tempo.time()
+list(map(lambda x : test2(),list(range(1000))))
+print(tempo.time()-t0)
